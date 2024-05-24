4eefd70af1c9df2ede89f0ee3bfdc46fe2f107f4
gg <gglearnpractice@gmail.com>
2024-05-24 15:04:58 +0000

    Updated comment

--- apps/comment/viewsets.py
+++ apps/comment/viewsets.py
@@ -2,12 +2,13 @@ from django.http.response import Http404
 
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import action
+from django.core.cache import cache
 
 from apps.abstract.viewsets import AbstractViewSet
 from apps.comment.models import Comment
 from apps.comment.serializers import CommentSerializer
 from apps.auth.permissions import UserPermission
-
 
 
 class CommentViewSet(AbstractViewSet):
@@ -33,9 +34,45 @@ class CommentViewSet(AbstractViewSet):
 
         return obj
 
+    def list(self, request, *args, **kwargs):
+        comment_objects = cache.get("comment_objects")
+
+        if comment_objects is None:
+            comment_objects = self.filter_queryset(self.get_queryset())
+            cache.set("comment_objects", comment_objects)
+
+        page = self.paginate_queryset(comment_objects)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(comment_objects, many=True)
+        return Response(serializer.data)
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    @action(methods=["post"], detail=True)
+    def like(self, request, *args, **kwargs):
+        comment = self.get_object()
+        user = self.request.user
+
+        user.like_comment(comment)
+
+        serializer = self.serializer_class(comment)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    @action(methods=["post"], detail=True)
+    def remove_like(self, request, *args, **kwargs):
+        comment = self.get_object()
+        user = self.request.user
+
+        user.remove_like_comment(comment)
+
+        serializer = self.serializer_class(comment)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
